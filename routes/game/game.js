fdf8559ed59e277da9c72d4473d6e55634009e95
joaquim <joaquimpineda@gmail.com>
2020-04-11 19:24:44 +0200

    fix

--- routes/game/game.js
+++ routes/game/game.js
@@ -12,7 +12,7 @@ const pinGenerator = async () => {
         let game = await Game.create({})
         game_id = game._id
 
-        await Actualgames.create({ pin, game_id})
+        await Actualgames.create({ pin, game_id })
 
     } else {
         //apartir de 7000 salas va EXTREMADAMENTE LENTO... NOSE SI SERIA MEJOR BAJARME TODO EL ARRAY EN LOCAL I HACER LA VALIDACION LOCALMENTE. 
@@ -24,30 +24,12 @@ const addToGame = () => {
 
 }
 
-
-router.post('/', async (req, res) => {
-    let { username, dificulty, categories, numberOfQuestions } = req.body
-    categories = JSON.parse(categories)
-    let arrayQuestions =await questionGenerator(numberOfQuestions, dificulty, categories)
-    let { pin, game_id } = await pinGenerator()
-    if (req.userId === null) {
-        await Game.findByIdAndUpdate(game_id,{questions:arrayQuestions,unloggedOwner:username})
-    } else {
-        await User.findById(req.userId, (err, resp) => { username = resp.username })
-        await Game.findByIdAndUpdate(game_id,{questions:arrayQuestions,owner:req.userId})
-    }
-
-    res.send({ pin, game_id }) //respondo con la info y despues actualizo el modelo de forma asyncrona
-});
-
-
-
 const questionGenerator = async (numberOfQuestions, dificulty, categories) => {
     let arrayQuestions = []
     let selectedQuestions = []
     arrayQuestions = await Questions.find({ category: categories, difficulty: dificulty }, { _id: 1 })
     if (arrayQuestions.length < numberOfQuestions) {
-        console.log(`pending to complete ${numberOfQuestions-arrayQuestions.length} questions`)
+        console.log(`pending to complete ${numberOfQuestions - arrayQuestions.length} questions`)
     }
     let numberResponseMongo = arrayQuestions.length
     for (let i = 0; i < numberOfQuestions - 1 && i < numberResponseMongo; i++) {
@@ -60,54 +42,23 @@ const questionGenerator = async (numberOfQuestions, dificulty, categories) => {
 
 
 
+router.post('/', async (req, res) => {
+    let { username, dificulty, categories, numberOfQuestions } = req.body
+    categories = JSON.parse(categories)
+    let arrayQuestions = await questionGenerator(numberOfQuestions, dificulty, categories)
+    let { pin, game_id } = await pinGenerator()
+    if (req.userId === null) {
+        await Game.findByIdAndUpdate(game_id, { questions: arrayQuestions, unloggedOwner: username })
+    } else {
+        await User.findById(req.userId, (err, resp) => { username = resp.username })
+        await Game.findByIdAndUpdate(game_id, { questions: arrayQuestions, owner: req.userId })
+    }
 
-
-// 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  let username = req.body.username
-// if (req.userId) {
-//     await User.findById(req.userId, (err, resp) => { username = resp.username })
-//     res.send("ok logged user")
-// } else {
-
-router.head('/join', async (req, res, next) => {  //<<--- validar username  (/:pincode/:username)
-    // let actualGame = await Actualgames({ pin: req.body.pin })
-    // if (actualGame == "") {
-    //     res.send("not valid pin")
-    // } else {
-    //     if (req.userId) {
-    //         //  await User.findById(req.userId, (err, resp) => { username = resp.username })
-    //         res.send("user con token nombre usuario siempre valido")
-    //     } else {
-
-    //         let response = await Game.findById(actualGame.game_id).populate("User")
-    //         if (response.noLogedUsers.indexOf(username) === -1 && response.users.username.indexOf()) {
-    //             res.status(200)
-    //         } else {
-    //             res.status(409).
-    //         }
-    //     }
-    // }
-
-
-    //    res.json({ response: "Get->/game/join" })   //return 201 (ok) o (409(si falla) + body (error: "String")) (ultimo)
+    res.send({ pin, game_id }) //respondo con la info y despues actualizo el modelo de forma asyncrona
 });
+
+
+
 
 
 router.post('/join', async (req, res) => {    //<<<--- (pincode----username)
