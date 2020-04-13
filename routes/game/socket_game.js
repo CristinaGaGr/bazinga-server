783a4e9b4b55f07b54943d6fad70ef05fc915b03
joaquim <joaquimpineda@gmail.com>
2020-04-14 01:02:02 +0200

    event Bye created

--- routes/game/socket_game.js
+++ routes/game/socket_game.js
@@ -89,9 +89,6 @@ const startListener = (socket, io) => {
 							UsersArray.push(element.username)
 						})
 						io.sockets.to(socket.room).emit('/user', UsersArray)
-						//nose como cojer la cookie! 
-
-						//falta incluir los usuarios logeados.
 
 					} else {
 						io.sockets.to(socket.room).emit('/user', [""])
@@ -103,93 +100,112 @@ const startListener = (socket, io) => {
 			}
 
 		})
+
+		socket.on("/bye", (user) => {
+			socket.leave(socket.room)
+			Game.findByIdAndUpdate(gameId, { $pull: { users: user } }, { new: true }, (err, gameResponse) => {
+				if (gameResponse !== null) {
+					let UsersArray = []
+					gameResponse.users.forEach(element => {
+						UsersArray.push(element.username)
+					})
+					io.sockets.to(socket.room).emit('/user', UsersArray)
+
+
+				}
+			})
+
+		})
+
+
+
+		socket.on("/start", () => {
+			try {
+				console.log(io.sockets.actualGame[socket.room].numberOfAnswers.numberOfPlayersAtRoom)
+				Actualgames.findOneAndDelete({ game_id: socket.room }, (err, res) => {
+				})
+				Game.findById(socket.room, async (err, gameResponse) => {
+					let response = await getNextCuestion(gameResponse.questions[gameResponse.questionNumber], gameResponse.questionNumber, gameResponse.questions.length)
+					console.log("1 question", response)
+					io.sockets.to(socket.room).in(socket.room).emit('/question', response)
+
+				})
+			} catch (error) {
+				console.log(error)
+
+			}
+
+		})
+
+
+		socket.on("/new-question", () => {
+			try {
+				socket.recivedAnswers++
+				socket.recivedAnswers = 0
+				Game.findByIdAndUpdate(socket.room, { $inc: { questionNumber: 1 } }, { new: true }, async (err, gameResponse) => {
+					if (gameResponse.questionNumber <= gameResponse.questions.length) {
+						let response = await getNextCuestion(gameResponse.questions[gameResponse.questionNumber], gameResponse.questionNumber, gameResponse.questions.length)
+						io.sockets.to(socket.room).in(socket.room).emit('/question', response)
+
+					}
+
+				})
+
+			} catch (error) {
+				console.log(error)
+
+			}
+
+		})
+
+
+
+
+		socket.on("/answer", async (questionId, answer, time) => {
+			try {
+				io.sockets.actualGame[socket.room].numberOfAnswers++
+				const currentGame = await Game.findById(socket.room);
+				let ranking
+				let points = 0
+				if (await checkCorrectAnswer(questionId, answer)) {
+					points = calculateAnswerScore(time)
+				}
+				if (currentGame.ranking.findIndex(obj => obj.user === socket.user.username) === -1) {
+					ranking = { user: socket.user.username, score: points }
+					currentGame.ranking.push(ranking)
+				} else {
+					currentGame.ranking[currentGame.ranking.findIndex(obj => obj.user === socket.user.username)].score += points
+				}
+				await currentGame.save();
+				let savedAnswer = { user: socket.user, question: questionId, responseTime: time, answer: answer, points: points }
+
+
+
+				if (io.sockets.actualGame[socket.room].numberOfAnswers === io.sockets.actualGame[socket.room].numberOfPlayersAtRoom) {
+					io.sockets.to(socket.room).emit("/correct-answer", await correctAnswer(questionId))
+					io.sockets.to(socket.room).emit("/ranking", currentGame.ranking)
+					io.sockets.actualGame[socket.room].numberOfAnswers = 0
+					if (currentGame.game.questionNumber === currentGame.questions.length) {
+						setTimeout(() => {
+							console.log()
+							delete io.sockets.actualGame[socket.room]
+						}, 1000 * 60);
+					}
+				} else {
+					console.log("answer recived waiting all user answer")
+				}
+				currentGame.savedAnswer.push(savedAnswer)
+				await currentGame.save()
+			} catch (error) {
+				console.log(error)
+
+			}
+
+		})
 	} catch (error) {
+		console.log("socket error")
 		console.log(error)
 	}
-
-	socket.on("/start", () => {
-		try {
-			console.log(io.sockets.actualGame[socket.room].numberOfAnswers.numberOfPlayersAtRoom)
-			Actualgames.findOneAndDelete({ game_id: socket.room }, (err, res) => {
-			})
-			Game.findById(socket.room, async (err, gameResponse) => {
-				let response = await getNextCuestion(gameResponse.questions[gameResponse.questionNumber], gameResponse.questionNumber, gameResponse.questions.length)
-				console.log("1 question", response)
-				io.sockets.to(socket.room).in(socket.room).emit('/question', response)
-
-			})
-		} catch (error) {
-			console.log(error)
-
-		}
-
-	})
-
-
-	socket.on("/new-question", () => {
-		try {
-			socket.recivedAnswers++
-			socket.recivedAnswers = 0
-			Game.findByIdAndUpdate(socket.room, { $inc: { questionNumber: 1 } }, { new: true }, async (err, gameResponse) => {
-				if (gameResponse.questionNumber <= gameResponse.questions.length) {
-					let response = await getNextCuestion(gameResponse.questions[gameResponse.questionNumber], gameResponse.questionNumber, gameResponse.questions.length)
-					io.sockets.to(socket.room).in(socket.room).emit('/question', response)
-					
-				}
-
-			})
-
-		} catch (error) {
-			console.log(error)
-
-		}
-
-	})
-
-
-
-
-	socket.on("/answer", async (questionId, answer, time) => {
-		try {
-			io.sockets.actualGame[socket.room].numberOfAnswers++
-			const currentGame = await Game.findById(socket.room);
-			let ranking
-			let points = 0
-			if (await checkCorrectAnswer(questionId, answer)) {
-				points = calculateAnswerScore(time)
-			}
-			if (currentGame.ranking.findIndex(obj => obj.user === socket.user.username) === -1) {
-				ranking = { user: socket.user.username, score: points }
-				currentGame.ranking.push(ranking)
-			} else {
-				currentGame.ranking[currentGame.ranking.findIndex(obj => obj.user === socket.user.username)].score += points
-			}
-			await currentGame.save();
-			let savedAnswer = { user: socket.user, question: questionId, responseTime: time, answer: answer, points: points }
-
-
-
-			if (io.sockets.actualGame[socket.room].numberOfAnswers === io.sockets.actualGame[socket.room].numberOfPlayersAtRoom) {
-				io.sockets.to(socket.room).emit("/correct-answer", await correctAnswer(questionId))
-				io.sockets.to(socket.room).emit("/ranking", currentGame.ranking)
-				io.sockets.actualGame[socket.room].numberOfAnswers = 0
-				if (currentGame.game.questionNumber === currentGame.questions.length) {
-					setTimeout(() => {
-						console.log()
-						delete io.sockets.actualGame[socket.room]	
-					}, 1000*60);
-				}
-			} else {
-				console.log("answer recived waiting all user answer")
-			}
-			currentGame.savedAnswer.push(savedAnswer)
-			await currentGame.save()
-		} catch (error) {
-			console.log(error)
-
-		}
-
-	})
 
 }
 
