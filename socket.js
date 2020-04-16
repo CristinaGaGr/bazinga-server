46adb9c1e3a2bdee31c57ac7ecfe6bf4eb22b4cb
joaquim <joaquimpineda@gmail.com>
2020-04-16 10:31:43 +0200

    fix console.error and semiclon

--- socket.js
+++ socket.js
@@ -1,27 +1,25 @@
 
 
-const socketGame = require('./routes/game/socket_game')
+const socketGame = require('./routes/game/socket_game');
 const connection = (io) => {
 	io.on('connection', socket => {
-		console.log("Number of conected users at socket:", io.engine.clientsCount, socket.user, socket.id)
+		console.log("Number of conected users at socket:", io.engine.clientsCount, socket.user, socket.id);
 
-		socketGame.startListener(socket, io)
+		socketGame.startListener(socket, io);
 		console.log('New user connected');
 		socket.on('disconnect', () => {
 			try {
-				io.sockets.actualGame[socket.room].numberOfPlayersAtRoom--
-		
-			
+				io.sockets.actualGame[socket.room].numberOfPlayersAtRoom--;
 			} catch (error) {
-
+				console.error(error)
 			}
 			console.log('User disconnected');
-		})
+		});
 
 	});
 
 };
 module.exports = {
 	connection
-}
+};
 
