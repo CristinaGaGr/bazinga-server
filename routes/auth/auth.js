f8d06a556512459b2d549081c972e9d0ba0834ae
joaquim <joaquimpineda@gmail.com>
2020-04-16 10:29:04 +0200

    auth js semiclon

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -5,14 +5,13 @@ const Jwt = require('jsonwebtoken');
 
 try {
 
-    function validateEmail(email) {
+    const validateEmail = (email) => {
         const re = /[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/g;
         return re.test(email);
-    }
+    };
 
     const setCookie = (res, user) => {
         const token = Jwt.sign({ _id: user._id, username: user.username }, process.env.PRIVATEKEY, { expiresIn: '30d' });
-        ('sendingcookie')
         res.cookie('bazinga', token, {
             maxAge: 43200000,
             httpOnly: false,
@@ -23,84 +22,84 @@ try {
 
     router.post('/logout', (req, res) => {
         res.clearCookie('bazinga');
-        res.json({ response: "Logout success" })
+        res.json({ response: "Logout success" });
     });
 
     router.post('/delete', (req, res) => {
         User.findOneAndDelete({ username: req.body.username }, (err, response) => {
             if (err) throw err;
             if (response === null) {
-                res.json({ action: "user not exist", response: response })
+                res.json({ action: "user not exist", response: response });
             } else {
-                res.json({ action: "user Deleted", response: response })
+                res.json({ action: "user Deleted", response: response });
             }
-        })
+        });
     });
 
 
     router.post('/signup', async (req, res) => {
         let { username, password, repeatPassword, email } = req.body;
-        console.log("signup", req.body)
-        let error = false
+        console.log("signup", req.body);
+        let error = false;
         let responsedb = await User.find({ username });
         if (responsedb.length !== 0) {
-            error = true
-            console.log("username exists")
-            res.status(401).send({ error: "Username alredy exists" })
+            error = true;
+            console.log("username exists");
+            res.status(401).send({ error: "Username alredy exists" });
         }
         if (!validateEmail(email)) {
-            error = true
-            console.log("email not valid")
-            res.status(401).send({ error: "Email not valid" })
+            error = true;
+            console.log("email not valid");
+            res.status(401).send({ error: "Email not valid" });
         }
 
         if (password !== repeatPassword) {
-            error = true
-            console.log("paswords not identical")
-            res.status(401).send({ error: "Confirm password fields are identical" })
+            error = true;
+            console.log("paswords not identical");
+            res.status(401).send({ error: "Confirm password fields are identical" });
         }
 
         if (error === false) {
-            console.log("all validated")
+            console.log("all validated");
             User.create({ username: username, password, email }, (err, respUser) => {
-                let { _id, username } = respUser
-                console.log(respUser)
+                let { _id, username } = respUser;
+                console.log(respUser);
                 setCookie(res, { _id, username });
-                res.json({ _id, username })
+                res.json({ _id, username });
             });
         }
     });
 
 
     router.post('/signin', (req, res) => {
-        let { username, password } = req.body
-        console.log("signup", req.body)
+        let { username, password } = req.body;
+        console.log("signup", req.body);
 
         User.findOne({ username }, (err, respUser) => {
             if (err) throw err;
             if (respUser === null) {
-                console.log("user not exist")
+                console.log("user not exist");
                 res.status(401).send({ error: "No valid password", login: false });
 
             } else {
                 respUser.comparePassword(password, function (err, isMatch) {
                     if (err) throw err;
                     if (isMatch) {
-                        let { _id, username } = respUser
-                        console.log("signup ok")
+                        let { _id, username } = respUser;
+                        console.log("signup ok");
                         setCookie(res, { _id, username });
                         res.status(200).json({ _id, username });
                     } else {
-                        console.log("no valid password")
+                        console.log("no valid password");
 
                         res.status(401).send({ error: "No valid password", login: false });
                     }
                 });
             }
-        })
+        });
     });
 } catch (err) {
-    console.error(err)
+    console.error(err);
 }
 
 module.exports = router;
